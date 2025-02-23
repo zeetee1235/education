7fec3cd15cda1121f1dbfbc4034d9e1ce7fd098e
darkeye1123 <junseo0508@gmail.com>
2025-02-23 22:38:23 +0900

    클래수

--- main.py
+++ main.py
@@ -10,3 +10,64 @@ GUI 필요없음
 3.2번째 숫자입력
 4.1번째 숫자와 2번째 숫자를 계산부호로 계산한 결과를 출력
 """
+"""class vari:
+    def insrt(self):
+    a=int(input('1st?'))
+    b=int(input('2nd?'))
+class cal:
+    def add(self):
+        result=a+b
+        return result
+    def min(self):
+        result=a+b
+        return result
+    def mul(self):
+        result=a+b
+        return result
+    def sub(self):
+        result=a+b
+        return result
+class kim(vari,cal):
+    pass
+h=kim()"""
+class calculator:
+    def __init__(self):
+        pass
+    def add(self,a,b):
+        return a+b 
+    def sub(self,a,b):
+        return a-b
+    def mul(self,a,b):
+        return a*b   
+    def div(self,a,b):
+         if b != 0:
+            return a / b
+         else:
+            return "error"
+class vari:
+    def __init__(self):
+        pass
+    def qu_num(self,text):
+        return int(input(text))
+    def qu_booho(self):
+        return input('what bohoo')
+
+
+
+    
+class fin(calculator,vari):
+    def run(self):
+        first=self.qu_num('첫번째?')
+        bohoo=self.qu_booho()
+        second=self.qu_num('두번째?')
+        if bohoo=='+':
+            result=self.add(first,second)
+        elif bohoo=='-':
+            result=self.sub(first,second)
+        elif bohoo=='*':
+            result=self.mul(first,second)
+        elif bohoo=='/':
+            result=self.div(first,second)
+        print(result)
+a=fin()   
+a.run()      
